f40c06e599a7865eed75d834eb2f076604d03ccb
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-26 14:50:47 +0200

    create navbar app

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -4,9 +4,15 @@ export default {
 }
 </script>
 <template>
-  <div>
-    ciao mondo
-  </div>
+  <nav class="navbar bg-light shadow">
+    <div class="container-fluid">
+      <a class="navbar-brand d-flex align-items-center" href="#">
+        <img src="https://img.yugioh-card.com/eu/wp-content/themes/yugioh/images/logo/Yugioh-EN-DE.svg" alt="Logo"
+          width="130" class="d-inline-block align-text-top">
+        Yu-Gi-Oh API
+      </a>
+    </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped></style>
